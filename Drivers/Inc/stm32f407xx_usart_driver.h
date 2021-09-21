98046d2debde8f39a54a652674b520bf62820f94
Xreath <samed_fazli@hotmail.com>
2021-09-21 05:26:08 +0300

    UART dirver complate and tested

--- Drivers/Inc/stm32f407xx_usart_driver.h
+++ Drivers/Inc/stm32f407xx_usart_driver.h
@@ -16,6 +16,69 @@
 /*
  * Configuration structure for USARTx peripheral
  */
+
+
+/*
+ *@USART_Mode
+ *Possible options for USART_Mode
+ */
+#define USART_MODE_ONLY_TX 	0
+#define USART_MODE_ONLY_RX 	1
+#define USART_MODE_TXRX  	2
+
+/*
+ *@USART_Baud
+ *Possible options for USART_Baud
+ */
+#define USART_STD_BAUD_1200					1200
+#define USART_STD_BAUD_2400					2400
+#define USART_STD_BAUD_9600					9600
+#define USART_STD_BAUD_19200 				19200
+#define USART_STD_BAUD_38400 				38400
+#define USART_STD_BAUD_57600 				57600
+#define USART_STD_BAUD_115200 				115200
+#define USART_STD_BAUD_230400 				230400
+#define USART_STD_BAUD_460800 				460800
+#define USART_STD_BAUD_921600 				921600
+#define USART_STD_BAUD_2M 					2000000
+#define USART_STD_BAUD_3M 					3000000
+
+
+/*
+ *@USART_ParityControl
+ *Possible options for USART_ParityControl
+ */
+#define USART_PARITY_EN_ODD   2
+#define USART_PARITY_EN_EVEN  1
+#define USART_PARITY_DISABLE   0
+
+/*
+ *@USART_WordLength
+ *Possible options for USART_WordLength
+ */
+#define USART_WORDLEN_8BITS  0
+#define USART_WORDLEN_9BITS  1
+
+/*
+ *@USART_NoOfStopBits
+ *Possible options for USART_NoOfStopBits
+ */
+#define USART_STOPBITS_1     0
+#define USART_STOPBITS_0_5   1
+#define USART_STOPBITS_2     2
+#define USART_STOPBITS_1_5   3
+
+
+/*
+ *@USART_HWFlowControl
+ *Possible options for USART_HWFlowControl
+ */
+#define USART_HW_FLOW_CTRL_NONE    	0
+#define USART_HW_FLOW_CTRL_CTS    	1
+#define USART_HW_FLOW_CTRL_RTS    	2
+#define USART_HW_FLOW_CTRL_CTS_RTS	3
+
+
 typedef struct
 {
 	uint8_t USART_Mode;
@@ -46,18 +109,37 @@ typedef struct
  **************************** USART FONCTIONS*************************
  */
 
+
 /*
  * USART Clock Control
  */
 void USART_Pclk_Ctrl(USART_RegDef_t *pUSARTx,uint8_t EnOrDi);
 void USART_PENB(USART_RegDef_t *pUSARTx,uint8_t EnorDi);
 
+/*
+ * Init and Deinit USART
+ */
+void USART_Init(USART_Handle_t *pUSARTHanlde);
+void USART_DeInit(USART_RegDef_t *pUSARTx);
+
+/*
+ * BaudRate Configuration
+ */
+void USART_SetBaudRate(USART_RegDef_t *pUSARTx, uint32_t BaudRate);
+/*
+ * Data Send and Receive
+ */
+void USART_SendData(USART_Handle_t *pUSARTHandle, uint8_t *pTxBuffer, uint32_t Len);
+void  USART_ReceiveData(USART_Handle_t *pUSARTHandle,uint8_t *pRxBuffer, uint32_t Len);
+uint8_t USART_SendDataIT(USART_Handle_t *pUSARTHandle,uint8_t *pTxBuffer, uint32_t Len);
+uint8_t USART_ReceiveDataIT(USART_Handle_t *pUSARTHandle,uint8_t *pRxBuffer, uint32_t Len);
 
 /*
  * USART Interrupt and config
  */
 void USART_IRQInterruptConfig(uint8_t IRQNumber, uint8_t EnorDi);
 void USART_IRQPriorityConfig(uint8_t IRQNumber,uint32_t IRQPriority);
+void USART_IRQHandling(USART_Handle_t *pHandle);
 
 
 /*
@@ -137,6 +219,22 @@ void USART_Clear_Flag(USART_RegDef_t *pUSARTx,uint16_t FlagName);
 #define USART_SR_LBD        			8
 #define USART_SR_CTS        			9
 
+/*
+ * Application states
+ */
+#define USART_BUSY_IN_RX 1
+#define USART_BUSY_IN_TX 2
+#define USART_READY 0
+
+
+#define USART_EVENT_TX_CMPLT   0
+#define USART_EVENT_RX_CMPLT   1
+#define USART_EVENT_IDLE       2
+#define USART_EVENT_CTS        3
+#define USART_EVENT_PE         4
+#define USART_ERR_FE     	   5
+#define USART_ERR_NE    	   6
+#define USART_ERR_ORE    	   7
 /*
  * USART FLAGS
  */
